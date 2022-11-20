b60397e2ebb6052d51ae8ec07c352405db24efc9
Dan Ceban <90266227+DanCeban01@users.noreply.github.com>
2022-11-20 11:57:57 +0200

    Delete CityPower.java

--- lab2/src/mafia_simulation/prototype/CityPower.java
+++
@@ -1,8 +0,0 @@
-package mafia_simulation.prototype;
-
-public abstract class CityPower extends Prototype {
-
-    @Override
-    public abstract CityPower clone() throws CloneNotSupportedException;
-
-}
