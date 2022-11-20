f7532282c0060910a37adcf8ed57a2d461c46e65
Dan Ceban <90266227+DanCeban01@users.noreply.github.com>
2022-11-20 11:58:28 +0200

    Delete Prototype.java

--- lab2/src/mafia_simulation/prototype/Prototype.java
+++
@@ -1,8 +0,0 @@
-package mafia_simulation.prototype;
-
-public abstract class Prototype implements Cloneable {
-
-    @Override
-    public abstract Object clone() throws CloneNotSupportedException;
-
-}
